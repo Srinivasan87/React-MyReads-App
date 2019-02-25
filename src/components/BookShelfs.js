90bd14b53ad8ad0e639a931b0b2548d2e029a5f3
Srinivasan Jayaraman <j_sreenevas@yahoo.com>
2019-02-25 14:43:12 -0500

    Update BookShelfs.js
    Removed static data

--- src/components/BookShelfs.js
+++ src/components/BookShelfs.js
@@ -2,28 +2,6 @@ import React from 'react'
 import '../App.css';
 import Books from './Books';
 
-
-const MyBooks = [
-    {
-        id: '1',
-        title: 'To Kill a Mockingbird',
-        authors: 'Harper Lee',
-        backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")'
-    },
-    {
-        id: '2',
-        title: 'Enders Game',
-        authors: 'Orson Scott Card',
-        backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")'
-    },
-    {
-        id: '3',
-        title: '1776',
-        authors: 'David McCullough',
-        backgroundImage: 'url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")'
-    },
-]
-
 function BookShelfs(props) {
     return (
         <div className="bookshelf">
@@ -31,7 +9,7 @@ function BookShelfs(props) {
                 <h2 className="bookshelf-title">{Shelf.title}</h2>
             ))}
             <div className="bookshelf-books">
-                <Books Books={MyBooks} />
+                <Books Books={props.MyBooks} />
             </div>
         </div>
     )
