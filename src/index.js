4e29ccebab12c14e29bff0182605e96ae0b36d90
Srinivasan Jayaraman <j_sreenevas@yahoo.com>
2019-02-28 22:09:45 -0500

    Update index.js
    Bug Fix

--- src/index.js
+++ src/index.js
@@ -4,5 +4,4 @@ import BooksApp from './App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
-{/* Adding BrowserRouter to listers for browser URL changes */ }
 ReactDOM.render(<BrowserRouter><BooksApp /></BrowserRouter>, document.getElementById('root'));
